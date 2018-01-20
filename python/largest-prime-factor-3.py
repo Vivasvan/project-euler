09301bd2b5cb13bed6ca169b56effbeb4c9ab0ab
Vivasvan Paranjape <viv@Vivasvans-MacBook-Pro.local>
2018-01-20 17:31:54 +0000

    xrange over range

--- python/largest-prime-factor-3.py
+++ python/largest-prime-factor-3.py
@@ -2,7 +2,7 @@ import math
 
 def largest_prime_factor(number):
   max = int(math.floor(math.sqrt(number)))
-  for n in range(max, 1, -1):
+  for n in xrange(max, 1, -1):
     if (number % n == 0 and is_prime(n)):
       return n
     else:
@@ -11,7 +11,7 @@ def largest_prime_factor(number):
 
 def is_prime(number):
   max = int(math.floor(math.sqrt(number)))
-  for n in range(2, max):
+  for n in xrange(2, max):
     if (number % n == 0):
       return False
     else:
