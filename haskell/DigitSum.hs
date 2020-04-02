255743b0933b045161346b0189e209c4f9d5822c
Viv <viv@getinconvo.com>
2020-04-02 21:11:12 +0100

    got a freebie, #20

--- haskell/DigitSum.hs
+++ haskell/DigitSum.hs
@@ -11,3 +11,12 @@ answer :: Int
 answer = sumDigits (2 ^ 1000)
 -- fails for big numbers, poss due to Int type being too small
 
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+factorialAnswer :: Int
+factorialAnswer = sumDigits (factorial 100)
+
+
+
