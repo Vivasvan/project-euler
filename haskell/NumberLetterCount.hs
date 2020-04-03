766f48c834197ad8167e00f6381ae254e6c2ff60
Viv <viv@getinconvo.com>
2020-04-03 20:21:16 +0100

    [17] rubbish attempt started

--- haskell/NumberLetterCount.hs
+++ haskell/NumberLetterCount.hs
@@ -5,20 +5,25 @@ import           Prelude
 -- name n | length (atomName n) > 0 == atomName  n
 
 hundredsPart n = n `div` 100
-hundreds n | n > 100   = atomName (hundredsPart n) ++ "hundred"
-           | otherwise = ""
+hundreds n | n `mod` 100 == 0 = atomName (hundredsPart n) ++ " hundred"
+           | n > 100          = atomName (hundredsPart n) ++ " hundred and "
+           | otherwise        = ""
 
 tensPart :: Int -> Int
 tensPart n = (n `mod` 100) `div` 10
-tens n | n > 9 && n <= 20 = atomName n
-       | n <= 9           = ""
-       | otherwise        = atomName (10 * tensPart n)
+tens n | n `mod` 100 > 9 && n `mod` 100 <= 20 = atomName (n `mod` 100) ++ " "
+       | n <= 9 = ""
+       | otherwise = atomName (10 * tensPart n) ++ " "
 
-units n = atomName (n `mod` 10)
-name n = hundreds n ++ tens n ++ units n
+units n | (n `mod` 100 > 19 || n `mod` 100 < 10) = atomName (n `mod` 10)
+        | otherwise                              = ""
+
+name n | n == 1000 = atomName n
+       | otherwise = hundreds n ++ tens n ++ units n
 
 allNamesLengthTest = length (concat (map name [1 .. 5]))
-allNamesLength = length (concat (map name [1 .. 1000]))
+allNamesLength =
+    length ((filter (\x -> x /= ' ')) (concat (map name [1 .. 1000])))
 
 atomName :: Int -> String
 atomName 1    = "one"
@@ -42,7 +47,7 @@ atomName 18   = "eighteen"
 atomName 19   = "nineteen"
 atomName 20   = "twenty"
 atomName 30   = "thirty"
-atomName 40   = "fourty"
+atomName 40   = "forty"
 atomName 50   = "fifty"
 atomName 60   = "sixty"
 atomName 70   = "seventy"
