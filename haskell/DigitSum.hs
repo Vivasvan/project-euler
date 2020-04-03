28643bd19711c4e475fdc596f23bee8458a71338
Viv <viv@getinconvo.com>
2020-04-03 11:26:29 +0100

    [25] smallest fib with d digits solved

--- haskell/DigitSum.hs
+++ haskell/DigitSum.hs
@@ -15,8 +15,14 @@ factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
+-- no 20
 factorialAnswer :: Int
 factorialAnswer = sumDigits (factorial 100)
 
+countDigits :: Integer -> Int
+countDigits n = length (show n)
 
-
+phi = (1 + sqrt 5) / 2
+smallestFibWithLengthD :: Int -> Int
+smallestFibWithLengthD d =
+    ceiling ((fromIntegral d - 1) / logBase 10 phi + logBase phi 5 / 2)
